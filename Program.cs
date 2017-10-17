61eaa3d3e4d2d6b5f5096d64ab55abac83c9876e
FIC <edusousa59@gmail.com>
2017-10-17 19:42:37 -0200

    Tabuada_du_mal

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DesvioFluxo_W3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k=1,h=5,j;
+            for(int i=0;i<=10;i++)
+            {
+                for(j=k;j<=h;j++)
+                {
+                    Console.Write(j+"X"+i+"="+j*i+"\t");
+                }
+                Console.WriteLine();
+                if(i==10 && j==6)
+                {
+                    i=0;
+                    k=6;
+                    h=10;
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
